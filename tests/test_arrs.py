58363e9d7649526337194738d51caa4c14fe75e0
Elena Pronichkina <prolena39@mail.ru>
2023-06-01 22:46:31 +0300

    добавлены варианты в тесты, покрытие 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,9 +4,18 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
-
+    assert arrs.get([1, 2, 3], -1, None) == None
+    assert arrs.get([1, 2, 3], 0, "test") == 1
+    assert arrs.get([], 1, None) == None
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([1, 2, 3], -1, 1) == []
+    assert arrs.my_slice([], 0) == []
+    assert arrs.my_slice([1, 2, 3], -2, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
+    assert arrs.my_slice([], -1, 0) == []
+    assert arrs.my_slice([1, 2, 3], 0, 3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3], -4) == [1, 2, 3]
